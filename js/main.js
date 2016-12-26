ebb26284e331a31b9a6c55766187b448f1e22289
Samuel Dost <samuel.dost@gmail.com>
2016-12-26 13:25:27 -0500

    Now drawing using WebGL!

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
+"use strict"
+
 var canvas = document.getElementById('cnvs');
-var context = canvas.getContext("2d");
 var generate = document.getElementById('generate');
 var solve = document.getElementById('solve');
 var width = document.getElementById('maze_width');
@@ -11,8 +12,7 @@ if(window.Worker) {
   var worker = new Worker("js/worker.js");
   generate.onclick = function() {
     disableButtons();
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    let data = context.createImageData(canvas.width, canvas.height);
+    let data = new ImageData(canvas.width, canvas.height);
     let message = {
       action: "gen_maze",
       values: {
@@ -26,7 +26,7 @@ if(window.Worker) {
   };
   solve.onclick = function() {
     disableButtons();
-    var data = context.createImageData(canvas.width, canvas.height);
+    var data = new ImageData(canvas.width, canvas.height);
     let message = {
       action: "solve_maze",
       values: {
@@ -37,9 +37,7 @@ if(window.Worker) {
   };
   worker.onmessage = function(e) {
     if (e.data.action === "render_maze") {
-      createImageBitmap(e.data.values.imageData).then(function(bitmap) {
-        context.drawImage(bitmap, 0, 0);
-      });
+      render(canvas, e.data.values.imageData);
       if (e.data.values.iterate) {
         let message = {
           action: "iterate_maze",
@@ -52,9 +50,7 @@ if(window.Worker) {
         enableButtons();
       }
     } else if (e.data.action === "render_solution") {
-      createImageBitmap(e.data.values.imageData).then(function(bitmap) {
-        context.drawImage(bitmap, 0, 0);
-      });
+      render(canvas, e.data.values.imageData);
       if (e.data.values.iterate) {
         let message = {
           action: "iterate_solution",
@@ -67,9 +63,7 @@ if(window.Worker) {
         enableButtons();
       }
     } else if (e.data.action === "render") {
-      createImageBitmap(e.data.values.imageData).then(function(bitmap) {
-        context.drawImage(bitmap, 0, 0);
-      });
+      render(canvas, e.data.values.imageData);
     }
   };
 }
@@ -79,7 +73,7 @@ function resizeCanvas() {
   canvas.height = window.innerHeight - 60;
 
   if(worker) {
-    var data = context.createImageData(canvas.width, canvas.height);
+    var data = new ImageData(canvas.width, canvas.height);
     let message = {
       action: "render",
       values: {
@@ -102,4 +96,102 @@ function enableButtons() {
   generate.enabled = true;
   solve.value = "Solve Maze";
   solve.enabled = true;
+}
+
+// GL helper functions
+
+function render(canvas, image) {
+  // Get A WebGL context
+  var gl = canvas.getContext("webgl");
+  if (!gl) {
+    return;
+  }
+  // setup GLSL program
+  var program = webglUtils.createProgramFromScripts(gl, ["2d-vertex-shader", "2d-fragment-shader"]);
+  // look up where the vertex data needs to go.
+  var positionLocation = gl.getAttribLocation(program, "a_position");
+  var texcoordLocation = gl.getAttribLocation(program, "a_texCoord");
+  // Create a buffer to put three 2d clip space points in
+  var positionBuffer = gl.createBuffer();
+  // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  // Set a rectangle the same size as the image.
+  setRectangle(gl, 0, 0, canvas.width, canvas.height);
+  // provide texture coordinates for the rectangle.
+  var texcoordBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, texcoordBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+      0.0,  0.0,
+      1.0,  0.0,
+      0.0,  1.0,
+      0.0,  1.0,
+      1.0,  0.0,
+      1.0,  1.0,
+  ]), gl.STATIC_DRAW);
+  // Create a texture.
+  var texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  // Set the parameters so we can render any size image.
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+  // Upload the image into the texture.
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+  // lookup uniforms
+  var resolutionLocation = gl.getUniformLocation(program, "u_resolution");
+  webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+  // Tell WebGL how to convert from clip space to pixels
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+  // Clear the canvas
+  gl.clearColor(0, 0, 0, 0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  // Tell it to use our program (pair of shaders)
+  gl.useProgram(program);
+  // Turn on the position attribute
+  gl.enableVertexAttribArray(positionLocation);
+  // Bind the position buffer.
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  // Tell the position attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+  var size = 2;          // 2 components per iteration
+  var type = gl.FLOAT;   // the data is 32bit floats
+  var normalize = false; // don't normalize the data
+  var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+  var offset = 0;        // start at the beginning of the buffer
+  gl.vertexAttribPointer(
+      positionLocation, size, type, normalize, stride, offset)
+  // Turn on the teccord attribute
+  gl.enableVertexAttribArray(texcoordLocation);
+  // Bind the position buffer.
+  gl.bindBuffer(gl.ARRAY_BUFFER, texcoordBuffer);
+  // Tell the position attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+  var size = 2;          // 2 components per iteration
+  var type = gl.FLOAT;   // the data is 32bit floats
+  var normalize = false; // don't normalize the data
+  var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+  var offset = 0;        // start at the beginning of the buffer
+  gl.vertexAttribPointer(
+      texcoordLocation, size, type, normalize, stride, offset)
+  // set the resolution
+  gl.uniform2f(resolutionLocation, gl.canvas.width, gl.canvas.height);
+  // Draw the rectangle.
+  var primitiveType = gl.TRIANGLES;
+  var offset = 0;
+  var count = 6;
+  gl.drawArrays(primitiveType, offset, count);
+}
+
+function setRectangle(gl, x, y, width, height) {
+  var x1 = x;
+  var x2 = x + width;
+  var y1 = y;
+  var y2 = y + height;
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+     x1, y1,
+     x2, y1,
+     x1, y2,
+     x1, y2,
+     x2, y1,
+     x2, y2,
+  ]), gl.STATIC_DRAW);
 }
