da403e1bd37254ee2ddfad417f4a446de7f22aeb
Samuel Dost <sdost@acorns.com>
2025-05-19 16:58:02 -0400

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -51,16 +51,4 @@ export default defineConfig({
     },
   },
   publicDir: 'public',
-  plugins: [
-    {
-      name: 'configure-response-headers',
-      configureServer: (server) => {
-        server.middlewares.use((_req, res, next) => {
-          res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
-          res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-          next();
-        });
-      },
-    },
-  ],
 });
